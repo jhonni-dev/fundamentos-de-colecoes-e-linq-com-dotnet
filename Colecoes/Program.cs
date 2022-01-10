d67311d86777f3c599df8524eb7c28d36ed03cd1
jhonni-dev <jhonni.dev@gmail.com>
2022-01-10 16:27:16 -0300

    Array multidimensional

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,10 +1,26 @@
-﻿int[] arrayInteiros = new int[3];
+﻿int[,] matriz = new int [4, 2] 
+{
+    {8, 8},
+    {10, 200},
+    {50, 100},
+    {90, 200}
+};
 
-arrayInteiros[0] = 10;
-arrayInteiros[1] = 20;
-arrayInteiros[2] = int.Parse("30");
+for (int i = 0; i < matriz.GetLength(0); i++) // Este for é para linha
+{
+    for (int j = 0; j < matriz.GetLength(1); j++) // Este for é para coluna
+    {
+        System.Console.WriteLine(matriz[i,j]); //Percorre linha e coluna
+    }
+}
 
-arrayInteiros[3] = 30; // Esta linha irá retornar um erro por tentar acessar um elemento que não existe
+
+//int[] arrayInteiros = new int[3];
+// arrayInteiros[0] = 10;
+// arrayInteiros[1] = 20;
+// arrayInteiros[2] = int.Parse("30");
+
+// arrayInteiros[3] = 30; // Esta linha irá retornar um erro por tentar acessar um elemento que não existe
 
 // System.Console.WriteLine("Percorrendo pelo for");
 // for (int i = 0; i < arrayInteiros.Length; i++)
