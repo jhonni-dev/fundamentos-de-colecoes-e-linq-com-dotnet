564c3c781ec3143512bce829c0c51bd56b7a1d2f
jhonni-dev <jhonni.dev@gmail.com>
2022-01-10 16:03:32 -0300

    Acessando um array de forma segura

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -2,16 +2,18 @@
 
 arrayInteiros[0] = 10;
 arrayInteiros[1] = 20;
-arrayInteiros[2] = 30;
+arrayInteiros[2] = int.Parse("30");
 
-System.Console.WriteLine("Percorrendo pelo for");
-for (int i = 0; i < arrayInteiros.Length; i++)
-{
-    System.Console.WriteLine(arrayInteiros[i]);
-}
+arrayInteiros[3] = 30; // Esta linha irá retornar um erro por tentar acessar um elemento que não existe
 
-System.Console.WriteLine("Percorrendo pelo foreach");
-foreach (int item in arrayInteiros)
-{
-    System.Console.WriteLine(item);
-}
+// System.Console.WriteLine("Percorrendo pelo for");
+// for (int i = 0; i < arrayInteiros.Length; i++)
+// {
+//     System.Console.WriteLine(arrayInteiros[i]);
+// }
+
+// System.Console.WriteLine("Percorrendo pelo foreach");
+// foreach (int item in arrayInteiros)
+// {
+//     System.Console.WriteLine(item);
+// }
